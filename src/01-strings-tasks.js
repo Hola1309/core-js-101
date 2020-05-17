8ac443f0ce8dd09ba3cbc70452c06e9b45452cff
Hola <hola131098@gmail.com>
2020-05-18 01:10:51 +0400

    feat: numbers-task solution

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -232,8 +232,9 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'object') return value instanceof String;
+  return typeof value === 'string';
 }
 
 
